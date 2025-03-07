3359149303997c6f32ce657eb3b8f659d8ad609a
r128 <111167388+r128w@users.noreply.github.com>
2025-03-07 11:48:02 -0500

    menu in main,js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
 window.onresize()
 
-
+var menuTimer;
 function initMenu(){
-    runMenu()
+    menuTimer = setInterval(runMenu, 16)
 }
 
 function initGame(){
+
+    clearTimeout(menuTimer)
+
+    pobjects = [];
+    planets = [];
 
     pobjects.push(new Player(-200, -200, 16))
     p = pobjects[0]
@@ -14,11 +19,17 @@ function initGame(){
     //temp
     pobjects.push(new Platform(-800, -900))
 
-    planets.push(new Planet(0, 0, 800, '#99eeaa'))// start
+    planets.push(new Planet(0, 0, 800, '#99eeaa'))// starts
+
+    for(var i = 0; i < 10; i++){
+        planets.push(new Planet(
+            (Math.random()-0.5)*10000, (Math.random()-0.5)*10000, (Math.random())*500+50
+        ))
+    }
     
 
     loadSprites()
-    runFrame()
+    frameTimer = setInterval(runFrame, 16)
 }
 
 
@@ -28,8 +39,6 @@ function runFrame(){
 
     iterateFrame()
     renderFrame()
-
-    frameTimer = setTimeout(runFrame, 16)
 }
 function halt(){clearTimeout(frameTimer)}
 
