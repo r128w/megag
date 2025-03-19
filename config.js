b822dee44cfa4a4569a29b5281c115e261334ef6
r128 <111167388+r128w@users.noreply.github.com>
2025-03-19 09:21:59 -0400

    multiplayer config options

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ var config = {
     drawPlanetInfluence: false,
     bigG: 0.08,
     gravLimit: 5000,
-    planetInfluenceFactor: 5
+    planetInfluenceFactor: 5,
+    playerMax: 10,
+    updateInterval: 500
 }
