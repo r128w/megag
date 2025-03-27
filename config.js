1760f82e74978cfc85d08f336368941bf527ed9d
r128 <111167388+r128w@users.noreply.github.com>
2025-03-27 13:35:34 -0400

    worldgen config

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var config = {
     minimap:{
         drawPlanetInfluence: false,
         pathPredictions:100,
-        pathPredictionResolution:10
+        pathPredictionResolution:10,
+        hellaZoom:false
     },
     resources:{
         mgMax:1,
@@ -11,10 +12,18 @@ var config = {
         genPeriod:2081// higher == less frequent, highly recommended to be prime or somewhat prime
     },
     bigG: 0.1,
+    worldGen:{
+        numPlanets:14,
+        spacing:4500,
+        minSpacing:1000,
+        minPSize:200,
+        maxPSize:600,
+        initSpread:7000
+    },
     gravLimit: 5000,
     planetInfluenceFactor: 5,
     playerMax: 10,
-    systemSize:30000,// how far out before things are deleted
+    systemSize:40000,// how far out before things are deleted
     sync:{
         updateInterval: 500,
         reconnectInterval: 10000
