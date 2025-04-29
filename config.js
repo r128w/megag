50c47c1b35a0ddbc83366179799b292c08863711
r128 <111167388+r128w@users.noreply.github.com>
2025-04-29 13:35:10 -0400

    config etc for subdocks

--- config.js
+++ config.js
@@ -32,12 +32,22 @@ var config = {
     },
     chatLength:128,
     buildings:{
-        names:["Base Platform", "Magnesium Mine", "Nitrate Farm", "Selenium Mine", "Basic Turret", "Miniturret", "Ammo Factory"],
-        reqs:[{mg:10,no3:5}, {mg:40,no3:50}, {mg:50,no3:40}, {mg:100,no3:120}, {mg:200,no3:150}, {mg:50,no3:25,se:75}, {mg:75,no3:10,se:25}],
-        buildtimes:[120, 480, 480, 960, 480, 1080, 960],
+        names:["Base Platform", "Magnesium Mine", "Nitrate Farm", "Selenium Mine", "Basic Turret", // ids 0-4
+            "Miniturret", "Ammo Factory", "Turret Dock", "Defense Dock", "Local Shield", // 5-9
+            "Planetary Shield"],// 10-14
+        reqs:[{mg:10,no3:5}, {mg:40,no3:50}, {mg:50,no3:40}, {mg:100,no3:120}, {mg:200,no3:150}, 
+            {mg:50,no3:25,se:75}, {mg:75,no3:10,se:25}, {mg:50,no3:80}, {mg:80,no3:50}, {mg:20,se:60},
+            {mg:45,se:125}],
+        buildtimes:[120, 480, 480, 960, 480,
+            1080, 960, 960, 960, 480,
+            1080],
         binds:['y', 'u', 'i', 'h', 'j', 'k'],// shared between all docks/menus
-        previews:[-1, 2, 4, 6, 8, 0, 8],// preview sprites for build menu
-        previewb:[-1, -1, -1, -1, 0, 1, -1]// preview barrels
+        previews:[-1, 2, 4, 6, 8,
+            0, 9, 10, 11, 0,
+            0],// preview sprites for build menu
+        previewb:[-1, -1, -1, -1, 0,
+            1, -1, -1, -1, -1,
+            -1]// preview barrels
     },
     bulstats:[
         {// base bullet
@@ -51,7 +61,7 @@ var config = {
             make:{cost:null,amount:null},
         },
         {// minigun bullet
-            firerate:5,damage:0.3,range:300,iv:8,spread:0.3,name:"Minigun",textureID:0,id:1,
+            firerate:3,damage:0.3,range:300,iv:8,spread:0.3,name:"Minigun",textureID:0,id:1,
             make:{cost:{mg:25,no3:30},amount:75}
         },
         {// bomb
